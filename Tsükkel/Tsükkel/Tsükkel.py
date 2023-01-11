7d5daccceaee77087d782e81f95ebe2b87671ce5
opilane <opilane@HP-CZC2349HTP>
2023-01-11 15:15:30 +0200

    jõulupuu

--- Tsükkel/Tsükkel/Tsükkel.py
+++ Tsükkel/Tsükkel/Tsükkel.py
@@ -3,7 +3,7 @@ from random import *
 
 
 
-
+#0
 n = input("Введите целое число: ")
 while type(n) != int:
     try:
@@ -16,42 +16,22 @@ else:
     print("Нечетное")
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#2ülesanne(6)
+#2 ülesanne(6)
 n=0
-print ("*kolmnurga")
+print ("kolmnurga")
 for e in range (11,0,-1):
     n = n + 1
-    for f in range (0, n+1):
+    for f in range (1, n+1):
         print ("*", end = "")
     print()
 print ("")
-
-
 
 #1 ülesanne(6)
 for x in range(5):
     print("******") 
 
-#ülesanne(6)
-#3
+#3 ülesanne(6)
+
 
 print ("kolmnurga")
 for g in range (11,0,-1):
@@ -60,5 +40,20 @@ for g in range (11,0,-1):
         print ("*", end = "")
     print()
 
-
-#0
+#-----------------------------
+for i in range(1,11):
+    print("*"*i,end="")
+    print()
+#---------------------------------
+for i in range(1,5):
+    x=str("*"*i).center(14," ")
+    print(x, end="")
+    print()
+for i in range(1,10):
+    x=str("*"*(i+2)).center(14," ")
+    print(x, end="")
+    print()
+for i in range(1,15):
+    x=str("*"*(i+4)).center(14," ")
+    print(x, end="")
+    print()
