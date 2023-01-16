b89a51ca24bf6c90acc520ff338d61a6e3e69e9f
opilane <opilane@HP-CZC2349HTP>
2023-01-16 10:02:51 +0200

    w

--- Tsükkel/Tsükkel/Tsükkel.py
+++ Tsükkel/Tsükkel/Tsükkel.py
@@ -1,8 +1,89 @@
 from math import *
 from random import *
+from winreg import *
+
+
+#1
+while True:
+    try:
+        nimi=input("Palun siseta oma nimi: ")
+        if nimi=="SIIM":
+            n=int(input("Palun siseta mitu krda soovin tervitus saada: "))#n=korda
+            for i in range(1, n+1): # i=katsete arv
+                print(f"Ole tervitatud, {nimi}, {i}. korda. ")
+        else:
+            break
+    except:
+        print("!!!")
 
 
 
+
+
+
+#------------------------------------------------------------------------
+#0===
+p=0
+while True:
+    number = int(input("Sisestage number suurem kui 10: "))
+    p+=1
+    if number >= 10:
+        print("Õigesti")
+        break
+    else:
+        print("Arv on liiga väike!")
+print("katsed", p)
+
+#---------------------------------------------------------------
+#22.2.0
+katsed=0
+answer=""
+while answer!="komm":
+    answer=input("Tahan kommi!")
+    katsed+=1
+print(f"katsed: {katsed}")
+print()
+
+
+
+
+#22
+print("Ma tahan kommi")
+katsed = 0
+while True:
+    answer = input("Tahan kommi!")
+    katsed += 1
+    if answer.lower() == "kommi":
+        print(f"Teil kommid kirjutakse kulus {katsed} katse.")
+        break
+
+#------------------------------------------------------------------------
+
+#11ülesane
+print("arvuti mõistatab nubrit 1-10 ja sina üritab seda ära arvata")
+a=randint(1,10)
+vastus=int(input("Mis arv on mõistatanud arvutit?: "))
+k=p=1 
+while vastus!=a:
+    print("Ära sa ei arvanud ära, proovi uuesti!: ")
+    vastus=int(input("Sisesta vastus: "))
+    k+=1
+    p+=1
+    if k>2:
+        print("Püüdlused on lõppenud")
+        b=input("Kas roovi veel kord: ")
+        if b.upper()=="JAH":
+            k=0
+            continue
+        else:
+            break
+if vastus==a:
+    print("Palju õnne, sa arvasid ära!",p )
+
+print()
+
+
+#------------------------------------------------------------------------
 #0
 n = input("Введите целое число: ")
 while type(n) != int:
@@ -15,6 +96,8 @@ if n % 2 == 0:
 else:
     print("Нечетное")
 
+
+#------------------------------------------------------------------------
 
 #2 ülesanne(6)
 n=0
@@ -40,6 +123,11 @@ for g in range (11,0,-1):
         print ("*", end = "")
     print()
 
+#------------------------------------------------------------------------
+
+
+
+
 #-----------------------------
 for i in range(1,11):
     print("*"*i,end="")
@@ -57,3 +145,8 @@ for i in range(1,15):
     x=str("*"*(i+4)).center(14," ")
     print(x, end="")
     print()
+
+
+
+
+
